48f0ae73f95d45694d43ac137b8cc7ae02da0d01
Keiichiro Sato <ksato@terrasky.co.jp>
2020-09-13 23:05:37 +0900

    あっとこーだ

---
+++ AtCoder/ABC178/A.java
@@ -0,0 +1,18 @@
+package AtCoder.ABC178;
+
+import java.util.*;
+
+public class A {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int x = sc.nextInt();
+
+        if (x == 0) {
+            System.out.println(1);
+        }else{
+            System.out.println(0);
+        }
+        sc.close();
+    }
+}
---
+++ AtCoder/ABC178/B.java
@@ -0,0 +1,30 @@
+package AtCoder.ABC178;
+
+import java.util.*;
+
+public class B {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        long a = sc.nextInt();
+        long b = sc.nextInt();
+        long c = sc.nextInt();
+        long d = sc.nextInt();
+        long max = 0;
+
+        max = a * c;
+
+        if (a*d >= max) {
+            max = a*d;
+        }
+        if (b*c>=max) {
+                max = b*c;
+        }
+        if (b*d>=max) {
+            max = b*d;
+        }
+            
+        System.out.println(max);
+        sc.close();
+    }
+}
