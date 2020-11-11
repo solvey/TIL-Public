ebcc415b5f65076ac30fd336f68ffa818b1c2cbb
Keiichiro Sato <ksato@terrasky.co.jp>
2020-11-11 21:54:44 +0900

    Easy 100続き

---
+++ AtCoder/CODE_FESTIVAL_2016/B.java
@@ -0,0 +1,15 @@
+package AtCoder.CODE_FESTIVAL_2016;
+import java.util.*;
+
+public class B {
+    Scanner sc = new Scanner(System.in);
+    int N = sc.nextInt();
+    int A = sc.nextInt();
+    int B = sc.nextInt();
+    int S[] = new int [N];
+    
+    for(int i=0; i<N; i++){
+        S[i] = sc.nextInt();
+    }
+
+}
