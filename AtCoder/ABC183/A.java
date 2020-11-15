6485107598cccdba26872c2b5f11c970878e551c
solvey <satokmh123@gmail.com>
2020-11-15 23:14:19 +0900

    ABC183

---
+++ AtCoder/ABC183/A.java
@@ -0,0 +1,16 @@
+package AtCoder.ABC183;
+import java.util.*;
+
+public class A {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int x = sc.nextInt();
+        if (x >= 0) {
+            System.out.println(x);
+        } else {
+            System.out.println("0");
+        }
+        sc.close();
+    }
+}
